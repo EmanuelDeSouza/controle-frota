17864440df2af5c3355f32864620f86f818ed0a2
EmanuelDeSouza <emanumaria@hotmail.com>
2025-11-23 18:06:35 -0300

    testefinal

--- backend/static/js/testecadastro.js
+++ backend/static/js/testecadastro.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:5000/api";
+
 document.addEventListener("DOMContentLoaded", () => {
     const formBtn = document.querySelector(".btn-cadastro");
     const listaBody = document.getElementById("lista-caminhoes-body");
