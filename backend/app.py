9dc0d5fafe744e7cc9f0f7e857d29c5e166420ac
EmanuelDeSouza <emanumaria@hotmail.com>
2026-06-13 14:32:24 -0300

    vaii

--- backend/app.py
+++ backend/app.py
@@ -130,4 +130,6 @@ app.register_blueprint(relatorio_bp)
 app.register_blueprint(dashboard_bp)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=not IS_PRODUCTION)
